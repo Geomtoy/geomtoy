c72eac19a6c3d6b0d86c1d18a8290e8a4abf4d64
FishEleven <mog1347@qq.com>
2022-01-17 11:58:37 +0800

    test: Add unit test and visual test devDependencies and configs
    -

---
+++ packages/view/karma.conf.js
@@ -0,0 +1,43 @@
+const { babel } = require("@rollup/plugin-babel");
+const { nodeResolve } = require("@rollup/plugin-node-resolve");
+const commonjs = require("@rollup/plugin-commonjs");
+
+const extensions = [".js", ".ts"];
+const exclude = /node_modules/;
+
+module.exports = function (config) {
+    config.set({
+        basePath: "",
+        frameworks: ["mocha", "chai"],
+        files: [
+            { pattern: "./test/**/*.test.ts", watched: false },
+            { pattern: "./test/visual/**", watched: false, included: false, nocache: false, served: true }
+        ],
+        exclude: [],
+        preprocessors: {
+            "**/*.ts": ["rollup"]
+        },
+        rollupPreprocessor: {
+            plugins: [nodeResolve({ extensions }), commonjs({ extensions, include: exclude }), babel({ babelHelpers: "bundled", extensions, exclude })],
+            onwarn(warning, rollupWarn) {
+                if (warning.code === "THIS_IS_UNDEFINED") return;
+                if (warning.code === "CIRCULAR_DEPENDENCY") return;
+                rollupWarn(warning);
+            },
+            output: {
+                format: "iife",
+                name: "view",
+                sourcemap: "inline"
+            }
+        },
+
+        reporters: ["progress"],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        browsers: ["Chrome"],
+        autoWatch: true,
+        singleRun: false,
+        concurrency: Infinity
+    });
+};
