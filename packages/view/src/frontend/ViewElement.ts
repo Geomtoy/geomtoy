48853bac30880e8372771da70f3cbaf72843a010
FishEleven <mog1347@qq.com>
2023-02-25 01:37:28 +0800

    feat(view): Simplify `ViewElement` related architecture
    -

--- packages/view/src/frontend/ViewElement.ts
+++ packages/view/src/frontend/ViewElement.ts
@@ -1,58 +1,140 @@
-import { Utility } from "@geomtoy/util";
-import { Shape } from "@geomtoy/core";
-import Stylable from "./Stylable";
-import type { Style, InteractiveStyle, PathLike } from "../types";
+import type { Shape } from "@geomtoy/core";
+import { Assert, Type, Utility } from "@geomtoy/util";
+import type { InteractiveStyle, PathInfo, Style } from "../types";
+import type SubView from "./SubView";
 import type View from "./View";
 
-export default class ViewElement extends Stylable {
-    private _interactable!: boolean;
-    private _autoUpdateView!: boolean;
-    private _uuid = Utility.uuid();
+const DEFAULT_STYLE: Style = {
+    paintOrder: "fill", // svg default
+    noFill: false,
+    noStroke: false,
+    fill: "black",
+    stroke: "black", // canvas default `black`, svg default `none`
+    strokeWidth: 1,
+    strokeDash: [],
+    strokeDashOffset: 0,
+    strokeLineJoin: "miter",
+    strokeLineCap: "butt",
+    strokeMiterLimit: 10 // canvas default 10, svg default 4
+};
+const DEFAULT_INTERACTIVE_STYLE: InteractiveStyle = {
+    fill: "black",
+    stroke: "black",
+    strokeWidth: 1
+};
 
-    private _autoUpdateFunction = function (this: ViewElement) {
-        if (this.parent) this.parent.render();
-    }.bind(this);
+export default class ViewElement {
+    private _interactable: boolean;
+    private _zIndex: number;
+    private _style = Utility.cloneDeep(DEFAULT_STYLE);
+    private _hoverStyle = Utility.cloneDeep(DEFAULT_INTERACTIVE_STYLE);
+    private _activeStyle = Utility.cloneDeep(DEFAULT_INTERACTIVE_STYLE);
 
-    parent?: View;
-    path?: PathLike;
+    shape: Shape;
+
+    paths: PathInfo[] = [];
+
+    // view: null && subView: null - ViewElement initial status
+    // view: View && subView: null - ViewElement directly added to a View
+    // view: View && subView: SubView - ViewElement added to a SubView
+    // @internal
+    view: View | null = null;
+    // @internal
+    subView: SubView | null = null;
 
     constructor(
-        public shape: Shape,
-        { interactable = false, autoUpdateView = true, style, hoverStyle, activeStyle } = {} as Partial<{
+        shape: Shape,
+        { interactable = false, zIndex = 0, style, hoverStyle, activeStyle } = {} as Partial<{
             interactable: boolean;
-            autoUpdateView: boolean;
+            zIndex: number;
             style: Partial<Style>;
             hoverStyle: Partial<InteractiveStyle>;
             activeStyle: Partial<InteractiveStyle>;
         }>
     ) {
-        super({ style, hoverStyle, activeStyle });
-        Object.assign(this, { interactable, autoUpdateView });
+        style !== undefined && this.style(style);
+        hoverStyle !== undefined && this.hoverStyle(hoverStyle);
+        activeStyle !== undefined && this.activeStyle(activeStyle);
+
+        this.shape = shape;
+        this._zIndex = zIndex;
+        this._interactable = interactable;
     }
 
-    get uuid() {
-        return this._uuid;
-    }
     get interactable() {
         return this._interactable;
     }
     set interactable(value) {
         this._interactable = value;
-        this.parent !== undefined && this.parent.refreshInteractables();
+        this.view?.refreshInteractables();
     }
-    get autoUpdateView() {
-        return this._autoUpdateView;
+
+    get zIndex() {
+        return this._zIndex;
     }
-    set autoUpdateView(value) {
-        this._autoUpdateView = value;
-        if (value) {
-            this.shape.on("any", this._autoUpdateFunction);
-        } else {
-            this.shape.off("any", this._autoUpdateFunction);
-        }
+    set zIndex(value) {
+        Assert.isInteger(value, "zIndex");
+        this._zIndex = value;
+        this.view?.sortElements();
     }
 
     move(deltaX: number, deltaY: number) {
         this.shape.move(deltaX, deltaY);
+        this.view?.requestRender();
+    }
+
+    style(): Style;
+    style(value: Partial<Style>): void;
+    style(value?: Partial<Style>) {
+        if (value === undefined) {
+            return Utility.cloneDeep(this._style);
+        }
+        value = Utility.cloneDeep(value);
+        if (value.strokeWidth !== undefined && !Type.isPositiveNumber(value.strokeWidth)) {
+            console.warn("[G]The `strokeWidth` is set unsuccessfully. For it should be a positive number. If you wish to set it to 0, consider setting the `stroke` to `transparent`.");
+            delete value.strokeWidth;
+        }
+        if (value.strokeMiterLimit !== undefined && !Type.isPositiveNumber(value.strokeMiterLimit)) {
+            console.warn("[G]The `strokeMiterLimit` is set unsuccessfully. For it should be a positive number.");
+            delete value.strokeMiterLimit;
+        }
+        if (value.strokeDashOffset !== undefined && !Type.isRealNumber(value.strokeDashOffset)) {
+            console.warn("[G]The `strokeDashOffset` is set unsuccessfully. For it should be a real number.");
+            delete value.strokeDashOffset;
+        }
+        if (value.strokeDash !== undefined && !value.strokeDash.every(n => Type.isRealNumber(n))) {
+            console.warn("[G]The `strokeDash` is set unsuccessfully. For it should be an array of real number.");
+            delete value.strokeDash;
+        }
+        Utility.assignDeep(this._style, value);
+        this.view?.requestRender();
+    }
+    hoverStyle(): InteractiveStyle;
+    hoverStyle(value: Partial<InteractiveStyle>): void;
+    hoverStyle(value?: Partial<InteractiveStyle>) {
+        if (value === undefined) {
+            return { ...this._hoverStyle };
+        }
+        value = { ...value };
+        if (value.strokeWidth !== undefined && !Type.isPositiveNumber(value.strokeWidth)) {
+            console.warn("[G]The `strokeWidth` is set unsuccessfully. For it should be a positive number. If you wish to set it to 0, consider setting the `stroke` to `transparent`.");
+            delete value.strokeWidth;
+        }
+        Object.assign(this._hoverStyle, value);
+        this.view?.requestRender();
+    }
+    activeStyle(): InteractiveStyle;
+    activeStyle(value: Partial<InteractiveStyle>): void;
+    activeStyle(value?: Partial<InteractiveStyle>) {
+        if (value === undefined) {
+            return { ...this._activeStyle };
+        }
+        value = { ...value };
+        if (value.strokeWidth !== undefined && !Type.isPositiveNumber(value.strokeWidth)) {
+            console.warn("[G]The `strokeWidth` is set unsuccessfully. For it should be a positive number. If you wish to set it to 0, consider setting the `stroke` to `transparent`.");
+            delete value.strokeWidth;
+        }
+        Object.assign(this._activeStyle, value);
+        this.view?.requestRender();
     }
 }
