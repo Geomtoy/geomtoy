a875a8a89e28f82d398ace1c57bd17f02976a10b
FishEleven <mog1347@qq.com>
2023-02-25 00:17:49 +0800

    feat(core): Add initializer hook in constructor for future extension
    -

--- packages/core/src/base/BaseObject.ts
+++ packages/core/src/base/BaseObject.ts
@@ -2,7 +2,7 @@ import { Utility } from "@geomtoy/util";
 
 export default abstract class BaseObject {
     private _uuid = Utility.uuid();
-    //user-defined data
+    // user-defined data
     private _data: { [key: string]: any } = {};
     /**
      * Get the class constructor `name` of this.
@@ -17,6 +17,22 @@ export default abstract class BaseObject {
         return this._uuid;
     }
 
+    constructor() {
+        let proto = Object.getPrototypeOf(this);
+        const initializers: (() => void)[] = [];
+
+        while (proto !== null) {
+            if (proto.constructor._initializer !== undefined) initializers.unshift(proto.constructor._initializer);
+            proto = Object.getPrototypeOf(proto);
+        }
+        for (const init of initializers) init.call(this);
+    }
+
+    private static _initializer?: () => void;
+    static addInitializer<T extends BaseObject>(initializer: (this: T) => void) {
+        this._initializer = initializer;
+    }
+
     data(key: string, value: any): this;
     data(key: string): any;
     data(key: string, value?: any) {
@@ -24,8 +40,7 @@ export default abstract class BaseObject {
         this._data[key] = value;
         return this;
     }
-
-    abstract toString(): string;
-    abstract toArray(): any[];
-    abstract toObject(): object;
+    toString() {
+        return `${this.name}(${this.uuid})`;
+    }
 }
