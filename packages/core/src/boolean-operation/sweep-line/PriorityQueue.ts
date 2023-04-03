f20a359a339431667578d9ed52e673e44ce76d89
FishEleven <mog1347@qq.com>
2023-04-03 09:07:56 +0800

    refactor(core): Refactor `PriorityQueue` code
    -

--- packages/core/src/boolean-operation/sweep-line/PriorityQueue.ts
+++ packages/core/src/boolean-operation/sweep-line/PriorityQueue.ts
@@ -1,99 +1,108 @@
-export default class PriorityQueue<T extends object> {
-    length;
+// Min Priority Queue
+export default class PriorityQueue<T extends any> {
+    private _data: T[] = [];
+    private _compareFn: (a: T, b: T) => number;
 
-    constructor(public data: T[] = [], public compareFn: (a: T, b: T) => -1 | 0 | 1) {
-        this.length = this.data.length;
-
-        if (this.length > 0) {
-            for (let i = (this.length >> 1) - 1; i >= 0; i--) this._down(i);
-        }
+    constructor(compareFn: (a: T, b: T) => number) {
+        this._compareFn = compareFn;
     }
+
+    get size() {
+        return this._data.length;
+    }
+
+    static fromArray<ST extends any>(arr: ST[], compareFn: (a: ST, b: ST) => number) {
+        const ret = new PriorityQueue(compareFn);
+        ret._data = [...arr];
+        if (ret._data.length > 0) {
+            for (let i = ret._data.length >> 1; i >= 0; i--) ret._siftDown(i);
+        }
+        return ret;
+    }
+    toArray() {
+        const copy = new PriorityQueue(this._compareFn);
+        copy._data = [...this._data];
+        const ret: T[] = [];
+        while (copy._data.length > 0) {
+            ret.push(copy.dequeue()!);
+        }
+        return ret;
+    }
+
     peek() {
-        return this.data[0];
+        return this._data[0];
     }
+
     dequeue() {
-        if (this.length === 0) return undefined;
-
-        const top = this.data[0];
-        const bottom = this.data.pop()!;
-
-        if (--this.length > 0) {
-            this.data[0] = bottom;
-            this._down(0);
+        if (this._data.length === 0) return undefined;
+        const ret = this._data[0];
+        const bItem = this._data.pop()!;
+        if (this._data.length > 0) {
+            this._data[0] = bItem;
+            this._siftDown(0);
         }
-        return top;
+        return ret;
     }
     enqueue(item: T) {
-        this.data.push(item);
-        this._up(this.length++);
+        this._data.push(item);
+        this._siftUp(this._data.length - 1);
     }
+
     update(item: T) {
-        const { data, compareFn } = this;
-        const pos = data.findIndex(i => item === i);
+        let pos = this._data.indexOf(item);
         if (pos === -1) return false;
-        const current = data[pos];
-        if (pos > 0) {
-            const parent = data[(pos - 1) >> 1];
-            if (compareFn(current, parent) < 0) {
-                this._up(pos);
-            } else {
-                this._down(pos);
-            }
+
+        const p = (pos - 1) >> 1; // parent
+        if (p < 0) {
+            this._siftDown(pos);
+            return true;
+        }
+        if (this._compareFn(item, this._data[p]) < 0) {
+            this._siftUp(pos);
         } else {
-            this._down(pos);
+            this._siftDown(pos);
         }
         return true;
     }
     remove(item: T) {
-        const { data } = this;
-        let pos = data.findIndex(i => item === i);
+        let pos = this._data.indexOf(item);
         if (pos === -1) return false;
         // force up the item to top
-        while (pos > 0) {
-            const parent = (pos - 1) >> 1;
-            const current = data[parent];
-            data[pos] = current;
-            pos = parent;
-        }
-        data[pos] = item;
+        this._siftUp(pos, true);
         this.dequeue();
         return true;
     }
 
     clear() {
-        this.data = [];
+        this._data = [];
     }
-    _up(pos: number) {
-        const { data, compareFn } = this;
-        const item = data[pos];
+
+    private _siftUp(pos: number, force = false) {
+        const item = this._data[pos];
 
         while (pos > 0) {
-            const parent = (pos - 1) >> 1;
-            const current = data[parent];
-            if (compareFn(item, current) >= 0) break;
-            data[pos] = current;
-            pos = parent;
+            const p = (pos - 1) >> 1; // parent
+            if (!force && this._compareFn(item, this._data[p]) >= 0) break;
+            this._data[pos] = this._data[p];
+            pos = p;
         }
-        data[pos] = item;
+        this._data[pos] = item;
     }
-    _down(pos: number) {
-        const { data, compareFn } = this;
-        const halfLength = this.length >> 1;
-        const item = data[pos];
+    private _siftDown(pos: number) {
+        const item = this._data[pos];
+        const h = this._data.length >> 1;
 
-        while (pos < halfLength) {
-            let bestChild = (pos << 1) + 1; // initially it is the left child
-            const right = bestChild + 1;
-
-            if (right < this.length && compareFn(data[right], data[bestChild]) < 0) {
-                bestChild = right;
+        while (pos < h) {
+            const l = (pos << 1) + 1; // left child
+            const r = (pos + 1) << 1; // right child
+            let m = l; // min of left and right child, initially left child
+            if (r < this._data.length && this._compareFn(this._data[r], this._data[l]) < 0) {
+                m = r;
             }
-            if (compareFn(data[bestChild], item) >= 0) break;
-
-            data[pos] = data[bestChild];
-            pos = bestChild;
+            if (this._compareFn(this._data[m], item) >= 0) break;
+            this._data[pos] = this._data[m];
+            pos = m;
         }
-
-        data[pos] = item;
+        this._data[pos] = item;
     }
 }
